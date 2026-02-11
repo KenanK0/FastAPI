55490475c76882669b71a6a78642db429e8e9219
KenanK0 <kfkasongo4@gmail.com>
2026-02-11 11:37:37 +0100

    finished all basic crud locally, no DB yet

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -27,6 +27,20 @@ def find_post(id):
             return item
 
 
+# def delete_post(id):
+#     post = find_post(id)
+#     if not post:
+#         return False
+#     my_posts.remove(post)
+
+#     return post
+
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
+
+
 @app.get("/")
 def root():
     return {"message": "Kenan Kasongo blabla"}
@@ -37,7 +51,7 @@ def get_posts():
     return {"data": my_posts}
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.model_dump()
     post_dict["id"] = randrange(0, 100000)
@@ -48,7 +62,38 @@ def create_posts(post: Post):
 
 
 @app.get("/posts/{id}")
-def get_post(id:int):
+def get_post(id: int, response: Response):
     p = find_post(id)
 
+    if not p:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with the id {id} was not found")
+
     return {"data": p}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_posts(id: int):
+    index = find_index_post(id)
+
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with the id {id} was not found")
+
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_404_NOT_FOUND)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_index_post(id)
+
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with the id {id} was not found")
+
+    post_dict = post.model_dump()
+    post_dict['id'] = id
+
+    my_posts[index] = post_dict
+    return {"data ": post_dict}
